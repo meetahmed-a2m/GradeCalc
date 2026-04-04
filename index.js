b57f9d86b000d33e08bc58a94bbaa3ae1204b0c5
meetahmed-a2m <devineprogrammer91@gmail.com>
2026-04-05 00:54:39 +0500

    assignment done

---
+++ index.js
@@ -0,0 +1,33 @@
+
+let getPercent = () => 
+{
+let tn = document.getElementById('TN').value;
+let on = document.getElementById('ON').value;
+let result = on/tn *100;
+if (tn ==="" || on === "")
+{
+document.getElementById('gradingBox').innerHTML = "Enter some value to calculate"
+return;
+}
+document.getElementById('answerBox').innerHTML = `Your total % is ${result.toFixed(2)}`
+
+if(result>=80)
+{
+document.getElementById('gradingBox').innerHTML = "Your Grade is A"
+}
+else if (result>70)
+{
+document.getElementById('gradingBox').innerHTML = "Your Grade is B"
+}
+else if (result>=50)
+{
+document.getElementById('gradingBox').innerHTML = "Your Grade is C"
+}
+else if (result<50)
+{
+document.getElementById('gradingBox').innerHTML = "Failed"
+}
+
+
+
+}
